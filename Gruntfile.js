feeb5e307606ccb5ccd043dec0cbea87091700be
益生菌 <yskun@outlook.com>
2017-07-19 13:40:26 +0800

    更改项目输出方式

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,44 @@
 /**
  * Created by yskun on 2017/4/18.
  */
+var ts = require('./tsconfig.json');
+ts.compilerOptions.declaration= true;
 
 module.exports = function (grunt) {
     grunt.initConfig({
         ts: {
             default: {
                 src: ["**/*.ts", "!node_modules/**", "!test/**"],
-                options: {
-                    module: 'commonjs',
-                    declaration: true,
-                    target: 'es6'
-                }
+                options: ts.compilerOptions
+            },
+            publish: {
+                src: ["**/*.ts", "!node_modules/**", "!test/**"],
+                options: ts.compilerOptions,
+                outDir: 'out'
             }
         },
         clean: {
-            build: {
-                src: ['**/*.d.ts', '**/*.js', '**/*.js.map','!node_modules/**', '!Gruntfile.js', '!test/**','.tscache']
+            default: {
+                src: ['**/*.d.ts', '**/*.js', '**/*.js.map', '!node_modules/**', '!Gruntfile.js', '!test/**', '.tscache']
+            },
+            publish: {
+                src: ['out/*','!out/.git']
+            }
+        },
+        copy: {
+            publish: {
+                files: [
+                    {extends:true,src:['package.json','README.md','LICENSE'],dest: 'out/'}
+                ]
             }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks("grunt-ts");
-    grunt.registerTask("default", ["clean", "ts"]);
+    grunt.registerTask("default", ["clean:default", "ts:default"]);
+    grunt.registerTask("publish", ["clean:publish", "ts:publish","copy:publish"]);
+
+
 
 };
