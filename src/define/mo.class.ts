47109195e3663b6ef97899e9a529573f129914b0
益生菌 <yskun@outlook.com>
2017-08-28 15:14:43 +0800

    修改 -> Mo 	debug现在由基类Mo原生提供

--- src/define/mo.class.ts
+++ src/define/mo.class.ts
@@ -1,27 +1,18 @@
 import * as debug from 'debug';
 
-
 /**
  * Mo 基础类，框架中所有类均基于Mo。
  */
 export abstract class Mo {
     public instance: string;
+    private _debug: debug.IDebugger = debug('Mo');
 
     constructor() {
         this.instance = this.constructor.name;
     }
-
-    private _debug: debug.IDebugger;
-
     get debug(): debug.IDebugger {
-        if (this._debug) {
-            this._debug.namespace = this.instance + ': MoServer';
-            return this._debug;
-        }
-    }
-
-    set debug(debug: debug.IDebugger) {
-        this._debug = debug;
+        this._debug.namespace = this.instance + ': MoServer';
+        return this._debug;
     }
 
 }
